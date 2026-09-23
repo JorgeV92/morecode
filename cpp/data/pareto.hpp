9c10ea1177a52c3d104d94f46054b79db2c2b890
JorgeV92 <jorgevmg31@gmail.com>
2026-09-23 16:36:51 -0500

    format

--- cpp/data/pareto.hpp
+++ cpp/data/pareto.hpp
@@ -1,3 +1,5 @@
+#pragma once 
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -11,39 +13,29 @@ std::vector<Point> quickStair(const std::vector<Point>& P, int l, int r) {
     if (l == r) {
         return {P[l]};
     }
-
     int mid = l + (r - l) / 2;
-
     std::vector<Point> L = quickStair(P, l, mid);
     std::vector<Point> R = quickStair(P, mid + 1, r);
-
     while (!L.empty() && L.back().y <= R[0].y) {
         L.pop_back();
     }
-
     std::vector<Point> result;
     result.reserve(L.size() + R.size());
-
     for (const Point& p : L) {
         result.push_back(p);
     }
-
     for (const Point& p : R) {
         result.push_back(p);
     }
-
     return result;
 }
 
 std::vector<Point> staircase(std::vector<int>& X, std::vector<int>& Y) {
     int n = X.size();
-
     std::vector<Point> P(n);
-
     for (int i = 0; i < n; ++i) {
         P[i] = {X[i], Y[i]};
     }
-
     sort(P.begin(), P.end(),
          [](const Point& a, const Point& b) {
              return a.x < b.x;
